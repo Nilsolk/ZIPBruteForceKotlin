7a70fe0294ff7502ff5bbfd91ea183b362b2a1d2
Nilsolk <nilsolk.bat@gmail.com>
2024-01-12 20:48:43 +0300

    Fix stackoverflow

--- src/main/kotlin/Decryption.kt
+++ src/main/kotlin/Decryption.kt
@@ -9,40 +9,29 @@ class Decryption(
     private val passwordLength = communication.getLength()
     private val letters: CharArray = when (communication.chooseAlphabet()) {
         true -> "1234567890".toCharArray()
-        //`~!@#$%^&*()_\\-+=[{]}|:;'\",<.>/?"
         false -> {
             println("Type your own characters below, without spaces")
             readln().toCharArray()
         }
     }
 
-
-    fun isPasswordCorrect(
-        rarFilePath: String, password: String = String(CharArray(passwordLength) { letters[0] }), passwordCount: Int = 1
-    ) {
-
-        println(password)
-//            val rarFile = File(rarFilePath)
-//            val archive = Archive(rarFile, password)
-//            println("Success password is -> $password")
-//            archive.close()
-        try {
-            val file = File(rarFilePath)
-            val document = PDDocument.load(file, password)
-            println("PDF-файл открыт успешно.")
-            document.close()
-
-//            } catch (e: Exception) {
-//                // Обработка ошибок при открытии PDF-файла или неправильном пароле
-//                println("Ошибка при открытии PDF-файла: ${e.message}")
-//            }
-
-        } catch (e: InvalidPasswordException) {
-            val tempCounter = passwordCount + 1
-            val newPassword = finder.findCombinationOfPassword(password.toCharArray(), letters)
-            isPasswordCorrect(rarFilePath, String(newPassword), tempCounter)
-        } catch (e: Exception) {
-            println("something went wrong,try again" + " ${e.message} + ${e.cause} + $e")
+    fun isPasswordCorrect(rarFilePath: String, initialPassword: String = String(CharArray(passwordLength) { letters[0] })) {
+        var password = initialPassword
+        while (true) {
+            println(password)
+            try {
+                val file = File(rarFilePath)
+                val document = PDDocument.load(file, password)
+                println("PDF-файл открыт успешно.")
+                document.close()
+                break
+            } catch (e: InvalidPasswordException) {
+                val newPassword = finder.findCombinationOfPassword(password.toCharArray(), letters)
+                password = String(newPassword)
+            } catch (e: Exception) {
+                println("something went wrong, try again" + " ${e.message} + ${e.cause} + $e")
+                break
+            }
         }
     }
 
